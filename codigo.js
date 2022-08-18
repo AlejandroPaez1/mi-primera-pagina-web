0866e82b5406109f8ea02095829451a935292ce0
alejandro911pa <alejandro.paez39@unach.mx>
2022-08-17 22:22:35 -0500

    first commit

---
+++ codigo.js
@@ -0,0 +1,27 @@
+//declaracion de variable 
+
+//variabletxt = "hoa mundo";
+//recipiente2 = 2;
+
+//se declara despues es mas rapido 
+let numero = 10;
+
+//se declarara al mismo tiempo 
+const numero2 = 5;
+
+//let esnulo = null;
+
+//alert(esnulo);
+//alert(nombre);
+
+let nombre = prompt ("cual es tu nombre");
+
+//alert ("tu nombre es"+ nombre);
+document.write("\n tu nombre es "+nombre);
+
+let resultado = numero / numero2;
+
+// document.write("\n "+ resultado);
+
+
+
